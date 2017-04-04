7a2e7838450ea5cc81a37ec82acff15f2e7f5751
cyndie <pier0172@algonquinlive.com>
2017-04-04 11:51:59 -0400

    Lab8 done

--- app/src/main/java/com/example/student/andoid/TestToolbar.java
+++ app/src/main/java/com/example/student/andoid/TestToolbar.java
@@ -1,24 +1,32 @@
 package com.example.student.andoid;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static com.example.student.andoid.R.id.menu_1;
 
 public class TestToolbar extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "TestToolBar";
     Context ctx;
-
-
+    private TextView newMessage;
+String message= "initial message";
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +36,11 @@ public class TestToolbar extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Hi there!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -50,13 +58,17 @@ public class TestToolbar extends AppCompatActivity {
      int id =   mi.getItemId();
 
         switch (id) {
-            case R.id.menu_1:
+            case menu_1:
                 Log.d("Toolbar", "Option 1 selected");
 
-                Toast.makeText(this, "You selected item 1", Toast.LENGTH_LONG).show();
-//                Toolbar toolbar = (Toolbar) findViewById(R.id.menu_1);
+              //  Toast.makeText(this, "You selected item 1", Toast.LENGTH_LONG).show();
+
+                Snackbar.make(fab, message, Snackbar.LENGTH_LONG).show();
+//                                .setAction("Action", null).show();
+
+//                Toolbar toolbar = (Toolbar) findViewById(R.id.menu_3);
 //                setSupportActionBar(toolbar);
-//
+
 //                FloatingActionButton menu1 = (FloatingActionButton) findViewById(R.id.menu_1);
 //                menu1.setOnClickListener(new View.OnClickListener() {
 //                    @Override
@@ -75,6 +87,9 @@ public class TestToolbar extends AppCompatActivity {
 // Add the buttons
                 builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+
+                        Intent intent = new Intent(TestToolbar.this, StartActivity.class);
+                        startActivityForResult(intent,5);
                         // User clicked OK button
 //                        public void onClick(DialogInterface dialog, int id) {
 //                            Log.i("Yes", "Yes");
@@ -94,38 +109,35 @@ public class TestToolbar extends AppCompatActivity {
                 break;
             case R.id.menu_3:
                 Log.d("Toolbar", "Option 3 selected");
-                //Start an activity…
-             //   mi = Toast.LENGTH_SHORT;
-               // Toast.makeText(this, "You selected item 3", Toast.LENGTH_LONG).show();
+                newLayoutDialog().show();
 
-                AlertDialog.Builder builder2 = new AlertDialog.Builder(ctx);
-                // Get the layout inflater
-              //  LayoutInflater inflater =  getActivity().getLayoutInflater(); ??????????????????????need
-
-                // Inflate and set the layout for the dialog
-                // Pass null as the parent view because its going in the dialog layout
-
-                //need?????????????????????????????????
-//                builder2.setView(inflater.inflate(R.layout.custom_dialog, null))
-//                        // Add action buttons
-//                        .setPositiveButton(R.string.signin, new DialogInterface.OnClickListener() {
+//                final AlertDialog.Builder builder3 = new AlertDialog.Builder(ctx);
+//                // Get the layout inflater
+//               LayoutInflater inflater =  TestToolbar.this.getLayoutInflater(); //??????????????????????need
+//
+//
+//
+//                builder3.setView(inflater.inflate(R.layout.custom_dialog, null))
+//                       //  Add action buttons
+//                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
 //                            @Override
 //                            public void onClick(DialogInterface dialog, int id) {
-//                                // sign in the user ...
+//
 //                            }
 //                        })
 //                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-//                            public void onClick(DialogInterface dialog, int id) {
-//                                MessageFragment.this.getDialog().cancel(); //  LoginDialogFragment
+//                           public void onClick(DialogInterface dialog, int id) {
+////                                TestToolbar.this.getDialog().cancel(); //  LoginDialogFragment
 //                            }
 //                        });
+//                //builder3.setView(  v );
+//                builder3.create().show();
 
                 break;
             case R.id.menu_4:
                 Log.d("Toolbar", "Option 3 selected");
-                //Start an activity…
-                //   mi = Toast.LENGTH_SHORT;
-                Toast.makeText(this, "Version 1.0, by Your Cyndie", Toast.LENGTH_LONG).show();
+
+                Toast.makeText(this, "Version 1.0, by Cyndie", Toast.LENGTH_LONG).show();
 
             break;
 
@@ -133,5 +145,28 @@ public class TestToolbar extends AppCompatActivity {
         return true;
     }
 
+
+    public Dialog newLayoutDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        final View dialogView = inflater.inflate(R.layout.custom_dialog, null);
+        builder.setView(dialogView);
+
+        final EditText newMessage = (EditText) dialogView.findViewById(R.id.newMessage);
+
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                message = newMessage.getText().toString();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //do nothing
+            }
+        });
+        return builder.create();
+    }
 
 }
